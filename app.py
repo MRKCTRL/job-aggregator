fe99785c38e5dd05b69177cb2b505b49e9e2c059
MRKCTRL <davidmbundu@gmail.com>
2025-03-02 07:13:29 +0200

    add new API keys to the code, want to implement other local jobs

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+from math import ceil 
+
 
 app=Flask(__name__)
 
@@ -9,6 +11,13 @@ LINKEDIN_API_KEY = ""
 
 INDEED_API_URL = ""
 LINKEDIN_API_URL = ""
+
+GLASSDOOR_API_KEY=""
+GLASSDOOR_PARTNER_ID=""
+GLASSDOOR_API_URL=""
+
+JOBS_PER_PAGE= 10
+
 
 
 def fetch_indeed_jobs(keyword, location):
@@ -38,10 +47,25 @@ def fetch_linkedin_jobs(keyword, location):
         return response.json().get("elements", [])
     return []
 
+def fetch_glassdoor_jobs(keyword, location):
+    params = {
+        "v": "1",
+        "t.p" : GLASSDOOR_PARTNER_ID,
+        "t.k" : GLASSDOOR_API_KEY,
+        "action" : "jobs",
+        "q" : keyword,
+        "l" : location 
+    }
+    response = requests.get(GLASSDOOR_API_URL, params=params)
+    if response.status_code == 200:
+        return response.json().get("response", {}).get("jobs", [])
+    return []
+
 
 @app("/", methods=["GET", "POST"])
 def index():
     jobs = []
+    page = requests.args.get("page", 1, type=int)
     if request.method == "POST":
         keyword = request.form.get("keyword")
         location = request.form.get("location")
@@ -49,12 +73,19 @@ def index():
         
         indeed_jobs = fetch_indeed_jobs(keyword, location)
         linkedin_jobs = fetch_linkedin_jobs(keyword, location)
+        glassdoor_jobs = fetch_glassdoor_jobs(keyword, location)
         
         
-        jobs = indeed_jobs + linkedin_jobs 
+        jobs = indeed_jobs + linkedin_jobs + glassdoor_jobs
         
+    
+    total_jobs = len(jobs)
+    total_pages = ceil(total_jobs / JOBS_PER_PAGE)
+    start = (page - 1) * JOBS_PER_PAGE
+    end = start + JOBS_PER_PAGE 
+    jobs_to_display=jobs[start:end]  
         
-    return render_template("index.html", jobs=jobs)
+    return render_template("index.html", jobs=jobs_to_display, page=page, total_pages=total_pages)
 
 if __name__ == "__main__":
     app.main(debug=True)
